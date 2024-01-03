0e43610d4c50d2b2ed1670b250179f2f54ed5d10
Mihail Donchev <mihaildonchev88@abv.bg>
2024-01-03 15:31:44 +0200

    Create 13.basic_insert.sql

---
+++ 02.Exercise Data Definition and Datatypes/13.basic_insert.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE `soft_uni`;
+
+USE `soft_uni`;
+
+CREATE TABLE `towns` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+INSERT INTO `towns` (`name`)
+VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+CREATE TABLE `addresses` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `address_text` VARCHAR(50) NOT NULL,
+    `town_id` INT
+);
+
+CREATE TABLE `departments` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(30) NOT NULL
+);
+
+INSERT INTO `departments` (`name`)
+VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+CREATE TABLE `eployees` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(50),
+    `middle_name` VARCHAR(50),
+    `last_name` VARCHAR(50),
+    `job_title` VARCHAR(50),
+    `departmen_id` INT,
+    `hire_date` DATE,
+    `salary` DOUBLE,
+    `address_id` INT
+);
+
+INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
+VALUES 
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
